edc0ede666c687bca23e3df4319af039d936cfe7
Азиз <aziiz0120@gmail.com>
2022-03-05 23:05:28 +0500

    build

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+    <button id="qwe" >sadasd</button>
     <router-view/>
 
   </div>
 </template>
 
+<script>
+const btn = document.getElementById('#qwe')
+btn.addEventListener('click', function () {
+   Notification.requestPermission();
+  // wait for permission
+})
+</script>
 
 <style lang="scss">
 
