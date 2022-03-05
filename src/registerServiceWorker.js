92ea6fcd4c856de1bde0f1b2dd13859e7cb0a76e
Азиз <aziiz0120@gmail.com>
2022-03-05 21:09:30 +0500

    build

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -14,10 +14,10 @@ if (process.env.NODE_ENV === 'production') {
             const websocket = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/');
             websocket.onopen = (e) => {
                 console.log(e)
-                setInterval(websocket.send('---------------'),1000)
             },
                 websocket.onmessage = (e) => {
                     console.log(e.data)
+                    setInterval(websocket.send('---------------'),1000)
 
 
                     // postMessage(e.data)
