65eaee95e43fb981a020b96faf691fbdf307e80a
Азиз <aziiz0120@gmail.com>
2022-03-05 20:35:20 +0500

    build

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,50 @@
 <template>
   <div id="app">
+
+    <h1>Web<br>Workers<br>basic<br>example</h1>
+
+    <div class="controls" tabindex="0">
+      <form @submit.prevent="onsubmit()">
+        <div>
+          <label for="number1">Multiply number 1: </label>
+          <input type="number" id="number1" value="0">
+        </div>
+        <div>
+          <label for="number2">Multiply number 2: </label>
+          <input type="number" id="number2" value="0">
+        </div>
+        <button type="submit">asdas</button>
+      </form>
+
+      <p class="result">Result: 0</p>
+    </div>
     <router-view/>
+
   </div>
 </template>
+
+<script>
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
+
+const result = document.querySelector('.result');
+
+if (window.Worker) {
+  const myWorker = new Worker("worker.js");
+
+  onsubmit = function () {
+    myWorker.postMessage([first.value, second.value]);
+    console.log('Message posted to worker');
+  }
+
+  myWorker.onmessage = function (e) {
+    result.textContent = e.data;
+    console.log('Message received from worker');
+  }
+} else {
+  console.log('Your browser doesn\'t support web workers.');
+}
+</script>
 
 <style lang="scss">
 
--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,30 +1,16 @@
 <template>
-  <div>
-    <h1>Web<br>Workers<br>basic<br>example</h1>
-
-    <div class="controls" tabindex="0">
-      <form>
-        <div>
-          <label for="number1">Multiply number 1: </label>
-          <input type="text" id="number1" value="0">
-        </div>
-        <div>
-          <label for="number2">Multiply number 2: </label>
-          <input type="text" id="number2" value="0">
-        </div>
-      </form>
-
-      <p class="result">Result: 0</p>
-    </div>
-  </div>
+<div>
+<h1>hello</h1>
+<h1>hello</h1>
+<h1>hello</h1>
+<h1>hello</h1>
+</div>
 </template>
 
 <script>
 export default {
   name: "OrderList"
 }
-
-
 </script>
 
 <style scoped>
